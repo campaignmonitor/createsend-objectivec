3082c030b0b876d3a050d40668ba2db3e625f61f
Nathan de Vries <nathan@atnan.com>
2012-03-27 17:33:44 +1100

    Implement Segment-related APIs.

--- Classes/CSAPI+Segments.h
+++ Classes/CSAPI+Segments.h
@@ -35,18 +35,161 @@
               completionHandler:(void (^)(NSArray* segments))completionHandler
                    errorHandler:(CSAPIErrorHandler)errorHandler;
 
-/*
+/**
+ Create a new segment for a specific list.
  
- TODO: These APIs are yet to be implemented
+     http://www.campaignmonitor.com/api/segments/#creating_a_segment
  
-   POST http://api.createsend.com/api/v3/segments/{listid}.{xml|json}
-    PUT http://api.createsend.com/api/v3/segments/{segmentid}.{xml|json}
-   POST http://api.createsend.com/api/v3/segments/{segmentid}/rules.{xml|json}
-    GET http://api.createsend.com/api/v3/segments/{segmentid}.{xml|json}
-    GET http://api.createsend.com/api/v3/segments/{segmentid}/active.{xml|json}?date={YYYY-MM-DD}&page={pagenumber}&...
- DELETE http://api.createsend.com/api/v3/segments/{segmentid}.{xml|json}
- DELETE http://api.createsend.com/api/v3/segments/{segmentid}/rules.{xml|json}
- 
+ @param listID ID of the list on which the segment should be created
+ @param title Title of the new segment
+ @param rules A dictionary of rules (or nil)
+ @param completionHandler Completion callback, with the ID of the newly created segment as the first and only argument
+ @param errorHandler Error callback
  */
+- (void)createSegmentWithListID:(NSString *)listID
+                          title:(NSString *)title
+                          rules:(NSDictionary *)rules
+              completionHandler:(void (^)(NSString* segmentID))completionHandler
+                   errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Update an existing segment.
+ 
+     http://www.campaignmonitor.com/api/segments/#updating_a_segment
+ 
+ @param segmentID ID of the segment to be updated
+ @param title Title of the segment
+ @param rules A dictionary of rules (or nil). Existing rules will be removed.
+ @param completionHandler Completion callback
+ @param errorHandler Error callback
+ */
+- (void)updateSegmentWithSegmentID:(NSString *)segmentID
+                             title:(NSString *)title
+                             rules:(NSDictionary *)rules
+                 completionHandler:(void (^)(void))completionHandler
+                      errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Add a new rule to an existing segment
+ 
+     http://www.campaignmonitor.com/api/segments/#adding_a_segment_rule
+ 
+ @param segmentID ID of the segment to which the rule will be added
+ @param rule Rule to add
+ @param completionHandler Completion callback
+ @param errorHandler Error callback
+ */
+- (void)addRuleToSegmentWithID:(NSString *)segmentID
+                          rule:(NSDictionary *)rule
+             completionHandler:(void (^)(void))completionHandler
+                  errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Remove all rules from an existing segment.
+ 
+     http://www.campaignmonitor.com/api/segments/#deleting_segment_rules
+ 
+ @param segmentID ID of the segment whose rules are to be deleted
+ @param completionHandler Completion callback
+ @param errorHandler Error callback
+ */
+- (void)removeAllRulesFromSegmentWithID:(NSString *)segmentID
+                      completionHandler:(void (^)(void))completionHandler
+                           errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Get the name, list ID, segment ID, number of active subscribers and rules for a segment.
+ 
+     http://www.campaignmonitor.com/api/segments/#getting_a_segment
+ 
+ @param segmentID ID of the segment to be retrieved
+ @param completionHandler Completion callback, with a segment dictionary as the
+ first and only argument. The dictionary is in the following format:
+ 
+     {
+       "SegmentID": "a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+       "ListID": "a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1",
+       "Title": "Segment Title",
+       "ActiveSubscribers": 0,
+       "Rules": [
+         {
+           "Subject": "Subject1",
+           "Clauses": [
+             "Clause1_1",
+             "Clause1_2"
+           ]
+         },
+         {
+           "Subject": "Subject2",
+           "Clauses": [
+             "Clause2_1"
+           ]
+         }
+       ]
+     }
+ 
+ @param errorHandler Error callback
+ */
+- (void)getSegmentDetailsWithSegmentID:(NSString *)segmentID
+                     completionHandler:(void (^)(NSDictionary* segmentDetails))completionHandler
+                          errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Get a paged result representing all of the active subscribers that match the rules for a specific segment.
+ 
+     http://www.campaignmonitor.com/api/segments/#getting_segment_subs
+ 
+ @param segmentID ID of the segment you want the active subscribers for
+ @param date Subscribers which became active on or after the date specified will be returned
+ @param page The page to retrieve
+ @param pageSize The number of subscribers to retrieve per page. Values accepted are between `10` and `1000`.
+ @param orderField The subscriber field to order the list by. Values accepted are `email`, `name` or `date`.
+ @param ascending Whether to sort the list (see `orderField`) in ascending order
+ @param completionHandler Completion callback, with a `CSPaginatedResult` as the first and only argument. Items in the result list are in the following format:
+ 
+     {
+       "EmailAddress": "subs+7t8787Y@example.com",
+       "Name": "Person One",
+       "Date": "2010-10-25 10:28:00",
+       "State": "Active",
+       "CustomFields": [
+         {
+           "Key": "website",
+           "Value": "http://example.com"
+         },
+         {
+           "Key": "age",
+           "Value": "24"
+         },
+         {
+           "Key": "subscription date",
+           "Value": "2010-03-09"
+         }
+       ]
+     }
+ 
+ @param errorHandler Error callback
+ */
+- (void)getActiveSubscribersWithSegmentID:(NSString *)segmentID
+                                  date:(NSDate *)date
+                                  page:(NSUInteger)page
+                              pageSize:(NSUInteger)pageSize
+                            orderField:(NSString *)orderField
+                             ascending:(BOOL)ascending
+                     completionHandler:(void (^)(CSPaginatedResult *))completionHandler
+                          errorHandler:(CSAPIErrorHandler)errorHandler;
+
+/**
+ Delete an existing segment from a subscriber list.
+ 
+     http://www.campaignmonitor.com/api/segments/#deleting_a_segment
+ 
+ @param segmentID ID of the segment to be deleted
+ @param completionHandler Completion callback
+ @param errorHandler Error callback
+ */
+- (void)deleteSegmentWithID:(NSString *)segmentID
+          completionHandler:(void (^)(void))completionHandler
+               errorHandler:(CSAPIErrorHandler)errorHandler;
 
 @end
--- Classes/CSAPI+Segments.m
+++ Classes/CSAPI+Segments.m
@@ -20,4 +20,104 @@
                    failure:errorHandler];
 }
 
+- (void)createSegmentWithListID:(NSString *)listID
+                          title:(NSString *)title
+                          rules:(NSDictionary *)rules
+              completionHandler:(void (^)(NSString* segmentID))completionHandler
+                   errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient postPath:[NSString stringWithFormat:@"segments/%@.json", listID]
+                 parameters:nil
+                 bodyObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             title, @"Title",
+                             rules, @"Rules", nil]
+                    success:completionHandler
+                    failure:errorHandler];
+}
+
+- (void)updateSegmentWithSegmentID:(NSString *)segmentID
+                             title:(NSString *)title
+                             rules:(NSDictionary *)rules
+                 completionHandler:(void (^)(void))completionHandler
+                      errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient putPath:[NSString stringWithFormat:@"segments/%@.json", segmentID]
+                parameters:nil
+                bodyObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                            title, @"Title",
+                            rules, @"Rules", nil]
+                   success:^(id response) { completionHandler(); }
+                   failure:errorHandler];
+}
+
+- (void)addRuleToSegmentWithID:(NSString *)segmentID
+                          rule:(NSDictionary *)rule
+             completionHandler:(void (^)(void))completionHandler
+                  errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient postPath:[NSString stringWithFormat:@"segments/%@/rules.json", segmentID]
+                 parameters:nil
+                 bodyObject:rule
+                    success:^(id response) { completionHandler(); }
+                    failure:errorHandler];
+}
+
+- (void)removeAllRulesFromSegmentWithID:(NSString *)segmentID
+                      completionHandler:(void (^)(void))completionHandler
+                           errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient deletePath:[NSString stringWithFormat:@"segments/%@/rules.json"]
+                   parameters:nil
+                      success:^(id response) { completionHandler(); }
+                      failure:errorHandler];
+}
+
+- (void)getSegmentDetailsWithSegmentID:(NSString *)segmentID
+                     completionHandler:(void (^)(NSDictionary* segmentDetails))completionHandler
+                          errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient getPath:[NSString stringWithFormat:@"segments/%@.json", segmentID]
+                parameters:nil
+                   success:completionHandler
+                   failure:errorHandler];
+}
+
+- (void)getActiveSubscribersWithSegmentID:(NSString *)segmentID
+                                     date:(NSDate *)date
+                                     page:(NSUInteger)page
+                                 pageSize:(NSUInteger)pageSize
+                               orderField:(NSString *)orderField
+                                ascending:(BOOL)ascending
+                        completionHandler:(void (^)(CSPaginatedResult *))completionHandler
+                             errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  NSMutableDictionary* queryParameters;
+  queryParameters = [[[CSAPI paginationParametersWithPage:page
+                                                 pageSize:pageSize
+                                               orderField:orderField
+                                                ascending:ascending] mutableCopy] autorelease];
+  
+  NSString* dateString = [[CSAPI sharedDateFormatter] stringFromDate:date];
+  [queryParameters setObject:dateString forKey:@"Date"];
+  
+  
+  [self.restClient getPath:[NSString stringWithFormat:@"segments/%@/active.json", segmentID]
+                parameters:queryParameters
+                   success:^(id response) {
+                     CSPaginatedResult* result = [CSPaginatedResult resultWithDictionary:response];
+                     completionHandler(result);
+                   }
+                   failure:errorHandler];
+}
+
+- (void)deleteSegmentWithID:(NSString *)segmentID
+          completionHandler:(void (^)(void))completionHandler
+               errorHandler:(CSAPIErrorHandler)errorHandler {
+  
+  [self.restClient deletePath:[NSString stringWithFormat:@"segments/%@.json", segmentID]
+                   parameters:nil
+                      success:^(id response) { completionHandler(); }
+                      failure:errorHandler];
+}
+
 @end
